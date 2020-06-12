2e0b1248e44f051d11de1cc6a24f0924a56ac83f
Gino Notto <ginojnotto@gmail.com>
2020-06-12 17:04:14 -0400

    move demo sim objects out of Sim3D

--- src/demos/demo1.ts
+++ src/demos/demo1.ts
@@ -1,4 +1,6 @@
 import { Sim3D } from "../engine/Sim3D";
+import { SimWall } from "../engine/objects/SimWall";
+import { SimDemoBlock } from "../engine/objects/SimDemoBlock";
 
 let simulator: Sim3D;
 
@@ -27,6 +29,19 @@ function main() {
       },
     },
   });
+
+  // DEMO - Add an angled wall to the scene
+  const angleWall = new SimWall(this.scene, this.world, {
+    start: { x: -5, y: 6 },
+    end: { x: 5, y: 8 },
+  });
+
+  this.addObject(angleWall);
+
+  // DEMO - Add a demo block to the screen
+  this.demoBlock = new SimDemoBlock(this.scene, this.world);
+  this.addObject(this.demoBlock);
+
   simulator.onresize();
   simulator.beginRendering();
 }
