8ff30dbccb9b1c60b0ae4071ff0124a9469d5811
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-21 16:21:59 +0200

    Create corresponding enums

---
+++ src/assets/convertUnit.ts
@@ -0,0 +1,16 @@
+// Main unit: centimeter
+export enum LengthUnit {
+  MILLIMETER = 0.1,
+  CENTIMETER = 1,
+  INCH = 2.54,
+  FEET = 30.48,
+  METER = 100,
+}
+
+// Main unit: degree
+export enum AngleUnit {
+  DEGREE = 1,
+  RADIAN = 57.3,
+}
+
+export default {};
