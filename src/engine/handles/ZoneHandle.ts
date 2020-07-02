1691676fefe31cfbf2f4ec7fc48cb4a7e3a256b2
Zhiquan Yeo <zyeo8@bloomberg.net>
2020-07-02 11:49:30 -0400

    Initial Zone implementation

---
+++ src/engine/handles/ZoneHandle.ts
@@ -0,0 +1,16 @@
+import { ObjectHandle } from "./ObjectHandle";
+import { Zone } from "../objects/Zone";
+
+export class ZoneHandle extends ObjectHandle<Zone> {
+  setColor(color: number): void {
+    this._rootObject.setColor(color);
+  }
+
+  setOpacity(opacity: number): void {
+    this._rootObject.setOpacity(opacity);
+  }
+
+  getZoneId(): string {
+    return this._rootObject.getZoneId();
+  }
+}
